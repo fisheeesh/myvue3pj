1db2ae28de6db95e3d73f1d4cd747e99ff36748a
fisheeesh <swanphyo444@gmail.com>
2024-09-06 15:45:41 +0700

    myvue3pj

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 ref="greet">{{ title }}</h1>
+  <button @click="onClickHandler">Click</button>
+  <Modal/>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  data(){
+    return{
+      title : 'Hello World'
+    }
+  },
+  methods:{
+    onClickHandler(){
+      this.$refs.greet.textContent = 'Hello!'
+    }
+  },
+  components:{
+    Modal
+  }
+};
+</script>
+
+<style scoped>
+h1{
+  color:red;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bckdrop">
+    <div class="modal">
+      <h1>Modal Title</h1>
+      <p>Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.bckdrop {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal {
+  background-color: white;
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  border-radius: 10px;
+}
+p {
+  font-style: normal;
+}
+</style>
